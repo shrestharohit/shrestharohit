bf32cb3cf88692534cd5992093ef99a2e936f20b
Rohit Shrestha <rohitshr98@gmail.com>
2026-05-14 18:51:02 +1000

    fix: update base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/shrestharohit/',
+  base: '/',
   server: {
     open: true, // optional, opens browser on start
   },
